aa20f24d5b07926424742099e3fa6439c739725c
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2026-05-20 17:38:43 -0600

    Permitir autenticacion de Sanctum mediante cabecera X-Authorization, getallheaders o parametros de consulta para evitar filtrado de cabeceras en produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,42 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->configureDefaults();
         \Carbon\Carbon::setLocale('es');
+
+        \Laravel\Sanctum\Sanctum::getAccessTokenFromRequestUsing(function (\Illuminate\Http\Request $request) {
+            // 1. Try regular bearer token
+            $token = $request->bearerToken();
+            if ($token) {
+                return $token;
+            }
+
+            // 2. Try 'X-Authorization' header
+            $xAuth = $request->header('X-Authorization');
+            if ($xAuth && str_starts_with($xAuth, 'Bearer ')) {
+                return substr($xAuth, 7);
+            }
+
+            // 3. Try checking raw headers from PHP if getallheaders is available
+            if (function_exists('getallheaders')) {
+                $headers = getallheaders();
+                foreach ($headers as $name => $value) {
+                    if (strtolower($name) === 'authorization' && str_starts_with($value, 'Bearer ')) {
+                        return substr($value, 7);
+                    }
+                }
+            }
+
+            // 4. Try query parameter 'token'
+            if ($token = $request->query('token')) {
+                return $token;
+            }
+
+            // 5. Try request input 'token'
+            if ($token = $request->input('token')) {
+                return $token;
+            }
+
+            return null;
+        });
     }
 
     protected function configureDefaults(): void
